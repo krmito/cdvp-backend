7bf9a511f3e01dbb75f884e466e1b9dab6b64009
Carlos Mina <carlosminnatoro@hotmail.com>
2026-02-25 15:27:25 -0500

    subir jugador mediante foto

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,7 +7,7 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Usuario, UserRole } from '@entities/usuario.entity';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
